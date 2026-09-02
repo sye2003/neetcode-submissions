f4a7ac948ee4b0f3406e2d87b090c92b64df2f4b
Syed Abudahir <146318899+sye2003@users.noreply.github.com>
2026-09-02 21:20:04 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+    if(s.length()!=t.length())
+    return false;
+
+    HashMap<Character,Integer> anaS = new HashMap<>();
+    HashMap<Character,Integer> anaT = new HashMap<>();
+
+    for(int i= 0 ;i<s.length();i++)
+    {
+
+    anaS.put(s.charAt(i),anaS.getOrDefault(s.charAt(i),0)+1);
+    anaT.put(t.charAt(i),anaT.getOrDefault(t.charAt(i),0)+1);
+
+    }
+
+    for(int i= 0 ;i<s.length();i++)
+    {
+        if(!anaS.get(s.charAt(i)).equals(anaT.get(s.charAt(i))))
+        return false;
+
+    }
+
+    return true;
+
+
+
+
+    }
+}
