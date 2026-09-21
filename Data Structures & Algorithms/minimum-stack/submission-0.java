5f50ba88a532a4765205f1ed386749ffe64351f2
Syed Abudahir <146318899+sye2003@users.noreply.github.com>
2026-09-21 19:56:45 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,51 @@
+class MinStack {
+
+
+   Stack<Integer> stack = new Stack<>();
+   Stack<Integer> minStack = new Stack<>();
+
+    // public MinStack() {
+
+        // stack = new Stack<>();
+        // minStack = new Stack<>();
+        
+    // }
+    
+    public void push(int val) {
+
+        stack.push(val);
+
+        if(minStack.isEmpty()){
+            minStack.push(val);
+
+        }
+        else{
+            if(val<=minStack.peek()){
+                minStack.push(val);
+            }
+        }
+        
+    }
+    
+    public void pop() {
+
+        if(stack.peek().equals(minStack.peek()))
+           minStack.pop();
+
+        stack.pop();
+       
+        
+    }
+    
+    public int top() {
+
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+
+        return minStack.peek();
+        
+    }
+}
